4c19587b4a19fad4521001d2cd14ca68e876fa71
Jobelle Lagonoy <lagonoyjobelle@gmail.com>
2024-05-07 17:57:28 +0800

    feat: filter products by outlet

--- backend/app/api/app/Http/Resources/ProductResource.php
+++ backend/app/api/app/Http/Resources/ProductResource.php
@@ -23,6 +23,7 @@ class ProductResource extends JsonResource
             'category_id' => $this->category_id,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
+            'category' => $this->whenLoaded('category'),
         ];
     }
 }
