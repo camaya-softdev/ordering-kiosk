4bfd5a2deebb551b578f43be2d26f8b8a9a3435d
Jobelle Lagonoy <lagonoyjobelle@gmail.com>
2024-05-08 15:50:54 +0800

    feat: confirm start over

--- frontend/app/ordering-kiosk/src/components/Common/BottomModal.jsx
+++ frontend/app/ordering-kiosk/src/components/Common/BottomModal.jsx
@@ -1,11 +1,35 @@
 import styles from "./Common.module.css";
 
-function BottomModal({ children, open, onClose, extra }) {
+function BottomModal({ 
+    children,
+    open, 
+    onClose, 
+    extras,
+    title,
+    subtitle
+}) {
     if(open){
         return (
-            <div className={styles.modalWrapper}>
-                <div className={styles.modalContent}>
-                    {children}
+            <div 
+                className={styles.modalWrapper}
+                onClick={onClose}
+            >
+                <div 
+                    className={styles.modalContent}
+                    onClick={(e) => e.stopPropagation()}
+                >
+                    <div className={styles.modalTitleWrapper}>
+                        <div className={styles.modalInnerWrapper}>
+                            <span className={styles.modalTitle}>{title}</span>
+                            <span className={styles.modalSubtitle}>{subtitle}</span>
+                        </div>
+                    </div>
+                    
+                    <div>{children}</div>
+
+                    <div className={styles.modalExtras}>
+                        {extras}
+                    </div>
                 </div>
             </div>
         );
