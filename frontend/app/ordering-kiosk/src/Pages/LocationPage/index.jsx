17ea29c68bf60478e1c7c935fccea03347fc3ddb
Markie33 <155521237+cctc-markchristianrubio@users.noreply.github.com>
2024-05-10 10:32:25 +0800

    location page

--- frontend/app/ordering-kiosk/src/Pages/LocationPage/index.jsx
+++ frontend/app/ordering-kiosk/src/Pages/LocationPage/index.jsx
@@ -1,7 +1,43 @@
 import styles from "./LocationPage.module.css";
 
-function LocationPage(){
-
+function LocationPage() {
+  <>
+    <div className={style.dineoptionFooter}>
+      <div className={style.topDetails}>
+        <div className={style.diningOptionTitle}>
+          <span className={style.spanTitle}>Dining option</span>
+          <span className={style.spanDetails}>DINE</span>
+        </div>
+        <div className={style.rowPerDetails}>
+          <span className={style.spanTitle}>Location</span>
+          <span className={style.spanDetails}>-</span>
+        </div>
+        <div className={style.rowPerDetails}>
+          <span className={style.spanTitle}>Table/Room Number</span>
+          <span className={style.spanDetails}>-</span>
+        </div>
+      </div>
+      <div className={style.bottomDetails}>
+        <div className={style.topDetails}>
+          <div className={style.rowPerDetails}>
+            <span className={style.spanTitle}>Number of order</span>
+            <span className={style.spanDetails}>-</span>
+          </div>
+          <div className={style.rowPerDetails}>
+            <span className={style.spanTotalTitle}>Total</span>
+            <span className={style.spanTotalDetails}>-</span>
+          </div>
+        </div>
+        <div className={style.buttonContainer}>
+          <div className={style.proceedPaymentBtn}>Choose payment method</div>
+          <div className={style.backOptionsBtn}>
+            <div className={style.startOverBtn}>Start over</div>
+            <div className={style.backBtn}>Back</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>;
 }
 
 export default LocationPage;
