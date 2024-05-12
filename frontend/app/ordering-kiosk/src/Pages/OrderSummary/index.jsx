7d313e3ca8fc35e4807ae2a462f1b23bb291a3ce
Markie33 <155521237+cctc-markchristianrubio@users.noreply.github.com>
2024-05-12 14:57:04 +0800

    Order summary page

---
+++ frontend/app/ordering-kiosk/src/Pages/OrderSummary/index.jsx
@@ -0,0 +1,161 @@
+import React from "react";
+import style from "./OrderSummary.module.css";
+import SummaryFooter from "../../components/Outletorder/SummaryFooter";
+import Progress from "../../components/DineOption/Progress";
+import armynavylogo from "../../assets/army-navy-logo.png";
+import classicburger from "../../assets/menuitem/plant-based-1.png";
+
+const OrderSummary = () => {
+  return (
+    <>
+      <Progress width={60} />
+      <div className={style.mainContainer}>
+        <p className={style.title}>Order Summary</p>
+        <div className={style.content}>
+          <div className={style.outletNameLogo}>
+            <img src={armynavylogo} alt="" srcset="" />
+            <span>Army Navy</span>
+          </div>
+          <div className={style.orderSummarylist}>
+            <div className={style.orderList}>
+              <div className={style.leftDetails}>
+                <p className={style.countList}>1.</p>
+                <div className={style.priceImg}>
+                  <img src={classicburger} />
+                  <div>
+                    <p>Classic burger</p>
+                    <span className={style.price}>P250</span>
+                  </div>
+                </div>
+              </div>
+              <div className={style.rightDetails}>
+                <div>x2</div>
+                <div className={style.quantityPrice}>P500</div>
+              </div>
+            </div>
+            <div className={style.orderList}>
+              <div className={style.leftDetails}>
+                <p className={style.countList}>1.</p>
+                <div className={style.priceImg}>
+                  <img src={classicburger} />
+                  <div>
+                    <p>Classic burger</p>
+                    <span className={style.price}>P250</span>
+                  </div>
+                </div>
+              </div>
+              <div className={style.rightDetails}>
+                <div>x2</div>
+                <div className={style.quantityPrice}>P500</div>
+              </div>
+            </div>
+            <div className={style.orderList}>
+              <div className={style.leftDetails}>
+                <p className={style.countList}>1.</p>
+                <div className={style.priceImg}>
+                  <img src={classicburger} />
+                  <div>
+                    <p>Classic burger</p>
+                    <span className={style.price}>P250</span>
+                  </div>
+                </div>
+              </div>
+              <div className={style.rightDetails}>
+                <div>x2</div>
+                <div className={style.quantityPrice}>P500</div>
+              </div>
+            </div>
+            <div className={style.orderList}>
+              <div className={style.leftDetails}>
+                <p className={style.countList}>1.</p>
+                <div className={style.priceImg}>
+                  <img src={classicburger} />
+                  <div>
+                    <p>Classic burger</p>
+                    <span className={style.price}>P250</span>
+                  </div>
+                </div>
+              </div>
+              <div className={style.rightDetails}>
+                <div>x2</div>
+                <div className={style.quantityPrice}>P500</div>
+              </div>
+            </div>
+            <div className={style.orderList}>
+              <div className={style.leftDetails}>
+                <p className={style.countList}>1.</p>
+                <div className={style.priceImg}>
+                  <img src={classicburger} />
+                  <div>
+                    <p>Classic burger</p>
+                    <span className={style.price}>P250</span>
+                  </div>
+                </div>
+              </div>
+              <div className={style.rightDetails}>
+                <div>x2</div>
+                <div className={style.quantityPrice}>P500</div>
+              </div>
+            </div>
+            <div className={style.orderList}>
+              <div className={style.leftDetails}>
+                <p className={style.countList}>1.</p>
+                <div className={style.priceImg}>
+                  <img src={classicburger} />
+                  <div>
+                    <p>Classic burger</p>
+                    <span className={style.price}>P250</span>
+                  </div>
+                </div>
+              </div>
+              <div className={style.rightDetails}>
+                <div>x2</div>
+                <div className={style.quantityPrice}>P500</div>
+              </div>
+            </div>
+            <div className={style.orderList}>
+              <div className={style.leftDetails}>
+                <p className={style.countList}>1.</p>
+                <div className={style.priceImg}>
+                  <img src={classicburger} />
+                  <div>
+                    <p>Classic burger</p>
+                    <span className={style.price}>P250</span>
+                  </div>
+                </div>
+              </div>
+              <div className={style.rightDetails}>
+                <div>x2</div>
+                <div className={style.quantityPrice}>P500</div>
+              </div>
+            </div>
+            <div className={style.orderList}>
+              <div className={style.leftDetails}>
+                <p className={style.countList}>1.</p>
+                <div className={style.priceImg}>
+                  <img src={classicburger} />
+                  <div>
+                    <p>Classic burger</p>
+                    <span className={style.price}>P250</span>
+                  </div>
+                </div>
+              </div>
+              <div className={style.rightDetails}>
+                <div>x2</div>
+                <div className={style.quantityPrice}>P500</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <SummaryFooter
+        showBackBtn={true}
+        showStartOver={true}
+        showDiningDetails={true}
+        showChoosePaymentBtn={true}
+      />
+    </>
+  );
+};
+
+export default OrderSummary;
--- frontend/app/ordering-kiosk/src/components/Outletorder/SummaryFooter.jsx
+++ frontend/app/ordering-kiosk/src/components/Outletorder/SummaryFooter.jsx
@@ -5,78 +5,69 @@ import Button from "../Common/Button";
 import styles from "./OutletOrder.module.css";
 
 function SummaryFooter({
-    continueToOrderOnClick, 
-    selectDineOptionOnClick,
-    startOverBtnOnClick,
-    backOnClick,
-    showContinueToOrder,
-    showSelectDineOption,
-    showStartOver,
-    showBackBtn,
-    showDiningDetails
+  continueToOrderOnClick,
+  selectDineOptionOnClick,
+  startOverBtnOnClick,
+  backOnClick,
+  showContinueToOrder,
+  showSelectDineOption,
+  showStartOver,
+  showBackBtn,
+  showDiningDetails,
+  showChoosePaymentBtn,
+}) {
+  const selectedProducts = useSelector((state) => state.order.selectedProducts);
 
-}){
-    const selectedProducts = useSelector(state => state.order.selectedProducts);
+  return (
+    <FooterLayout className={styles.viewOrderFooter}>
+      <div className={styles.viewOrderFooterInner}>
+        <div className={styles.viewOrderFooterDetails}>
+          {showDiningDetails && (
+            <p>
+              <span>Dining Option</span>
+              <span>-</span>
+            </p>
+          )}
+          {showDiningDetails && <hr></hr>}
+          <p>
+            <span>Number of order</span>
+            <span>{selectedProducts.length}</span>
+          </p>
 
-    return(
-        <FooterLayout className={styles.viewOrderFooter}>
-            <div className={styles.viewOrderFooterInner}>
-                <div className={styles.viewOrderFooterDetails}>
-                    {
-                        showDiningDetails &&
-                        <p>
-                            <span>Dining Option</span>
-                            <span>-</span>
-                        </p>
-                    }
-                    {
-                        showDiningDetails &&
-                        <hr></hr>
-                    }
-                    <p>
-                        <span>Number of order</span>
-                        <span>{selectedProducts.length}</span>
-                    </p>
+          <p className={styles.viewOrderFooterBold}>
+            <span>TOTAL</span>
+            <span>
+              PHP {formatNumber(calculateTotalPrice(selectedProducts))}
+            </span>
+          </p>
+        </div>
 
-                    <p className={styles.viewOrderFooterBold}>
-                        <span>TOTAL</span>
-                        <span>PHP {formatNumber(calculateTotalPrice(selectedProducts))}</span>
-                    </p>
-                </div>
+        <div>
+          {showContinueToOrder ? (
+            <Button type="white" onClick={continueToOrderOnClick}>
+              Continue to order
+            </Button>
+          ) : null}
 
-                <div>
-                    {
-                        showContinueToOrder ? 
-                        <Button type="white" onClick={continueToOrderOnClick}>
-                            Continue to order
-                        </Button>:null
-                    }
+          {showSelectDineOption ? (
+            <Button type="black" onClick={selectDineOptionOnClick}>
+              Proceed to checkout
+            </Button>
+          ) : null}
+          <div>
+            {showChoosePaymentBtn ? (
+              //   <Button type="white" onClick={choosePaymentOnclick}>
+              <Button type="white">Choose payment method</Button>
+            ) : null}
+          </div>
 
-                    {
-                        showSelectDineOption ?
-                        <Button type="black" onClick={selectDineOptionOnClick}>
-                            Proceed to checkout
-                        </Button>:null
-                    }
+          {showStartOver ? <Button type="gray">Start over</Button> : null}
 
-                    {
-                        showStartOver ?
-                        <Button type="gray" >
-                            Start over
-                        </Button>:null
-                    }
-
-                    {
-                        showBackBtn ?
-                        <Button type="white" >
-                            Back
-                        </Button>:null
-                    }
-                </div>
-
-            </div>
-        </FooterLayout>
-    );
+          {showBackBtn ? <Button type="white">Back</Button> : null}
+        </div>
+      </div>
+    </FooterLayout>
+  );
 }
 
 export default SummaryFooter;
