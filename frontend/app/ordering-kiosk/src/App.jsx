a0fa9c83321a13e92b2d06d14d7f68e7849e279e
Jobelle Lagonoy <lagonoyjobelle@gmail.com>
2024-05-22 19:06:56 +0800

    fix: cookie problems

--- frontend/app/ordering-kiosk/src/App.jsx
+++ frontend/app/ordering-kiosk/src/App.jsx
@@ -1,9 +1,14 @@
+import { useEffect } from "react";
 import "./App.css";
 import "./index.css";
 import {MainLayout} from "./layout/MainLayout";
+import { checkCookieValidity } from "./utils/Common/CheckCookieValidity";
 
 function App() {
   
+  useEffect(() => {
+    checkCookieValidity();
+  }, []);
 
   return (
     <>
