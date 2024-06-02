ebfc09a55db59a4ea66e90366b94ff748b977bfc
Markie33 <155521237+cctc-markchristianrubio@users.noreply.github.com>
2024-06-02 16:23:01 +0800

    Feat: disable inspect element

--- frontend/app/ordering-kiosk/src/App.jsx
+++ frontend/app/ordering-kiosk/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import "./App.css";
 import "./index.css";
-import {MainLayout} from "./layout/MainLayout";
+import { MainLayout } from "./layout/MainLayout";
 import { checkCookieValidity } from "./utils/Common/CheckCookieValidity";
 import { useDispatch } from "react-redux";
 
@@ -9,11 +9,32 @@ function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     checkCookieValidity(dispatch);
-  }, []);
+
+    const disableContextMenu = (e) => {
+      e.preventDefault();
+    };
+
+    const disableShortcuts = (e) => {
+      if (
+        (e.ctrlKey && e.shiftKey && (e.key === "I" || e.key === "J")) || // Ctrl+Shift+I or Ctrl+Shift+J
+        e.key === "F12" // F12
+      ) {
+        e.preventDefault();
+      }
+    };
+
+    document.addEventListener("contextmenu", disableContextMenu);
+    document.addEventListener("keydown", disableShortcuts);
+
+    return () => {
+      document.removeEventListener("contextmenu", disableContextMenu);
+      document.removeEventListener("keydown", disableShortcuts);
+    };
+  }, [dispatch]);
 
   return (
     <>
-      <MainLayout/>
+      <MainLayout />
     </>
   );
 }
